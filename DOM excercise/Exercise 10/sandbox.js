abe585e18daf24f1b638ec46b3728284331508ff
okedo <eliahmwelangi01@gmail.com>
2023-09-21 15:13:16 +0300

    exercise

---
+++ DOM excercise/Exercise 10/sandbox.js
@@ -0,0 +1,11 @@
+const list = document.getElementById('list');
+// const listItems  = Array.from(list.children);
+
+// listItems.forEach( items => {
+    items.addEventListener('mousemove', e => {
+       console.log(e.target);
+    })
+// })
+
+
+
